2ffb39e135c807ced181c1f977c3df76381b0f88
leobozzi <lbozzi@gmail.com>
2022-11-01 08:33:31 -0300

    Inititla commit

---
+++ garaje/models/garaje.aparcamiento.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from typing_extensions import Required
+from odoo import api, fields, models
+from odoo.tools.translate import _
+
+
+class GarajeAparcamiento(models.Model):
+    _name = 'garaje.aparcamiento'
+    _description = "garaje.aparcamiento"
+
+    name = fields.Char(
+        string="Dirección",
+        required=True,
+    )
+    plazas=fields.Integer(
+        string="Plazas",
+        required=True,
+    )
