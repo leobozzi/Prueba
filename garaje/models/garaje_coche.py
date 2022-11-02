b735a16ba3719084c51c2e6298be379af7060f04
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2022-11-02 10:08:08 -0300

    Modificacion - campo consumo

--- garaje/models/garaje_coche.py
+++ garaje/models/garaje_coche.py
@@ -28,13 +28,13 @@ class GarajeCoche(models.Model):
     construido = fields.Date(
         string="Fecha del modelo",
     )
-    consumo = fields.Float('Consumo', (4,1), default= 0.0, help ='Consumo promedio de cada 100 kms')
-    #consumo = fields.Float(
-        #(4,1), #limit
-        #string='Consumo',
-        #default= 0.0, 
-        #help ='Consumo promedio de cada 100 km',
-    #)
+
+    consumo = fields.Float(
+        'Consumo', #string -> no permite la etiqueta string al ser float.
+        (4,1), #limit
+        default= 0.0, 
+        help ='Consumo promedio de cada 100 kms'
+    )
     averiado = fields.Boolean(
         string = "Averiado",
         default = False
