c78f57dc7be299980b1cae3bb13748977c1dbc2c
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2022-11-11 09:00:10 -0300

    agregado - manifest - data - security

--- sportservice/__manifest__.py
+++ sportservice/__manifest__.py
@@ -22,10 +22,12 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/sportservice_security.xml',
+        'security/ir.model.access.csv',
         'views/sportservice_cliente_views.xml',
         'views/sportservice_views.xml',
         'views/templates.xml',
+       
     ],
 
     'installable': True,
